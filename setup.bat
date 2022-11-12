4b6f81f1575fe40e13005ad6cce3b091d509fd18
Frakist <xgamerxthegamer@gmail.com>
2022-11-12 17:34:48 +0200

    setup for flixel shit

---
+++ setup.bat
@@ -0,0 +1,20 @@
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+haxelib install hxCodec
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+
+haxelib run lime setup flixel
+
+haxelib run lime setup
+
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+
+haxelib update flixel
+
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+
+haxelib install hscript
+haxelib install flixel-ui
+haxelib install newgrounds
