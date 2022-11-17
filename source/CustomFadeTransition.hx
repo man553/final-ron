18db81cbd4a74475f01af2cd811ed8a264b831e5
Frakist <xgamerxthegamer@gmail.com>
2022-11-17 18:16:31 +0200

    new transition effect (broken)

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -3,15 +3,22 @@ package;
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.addons.ui.FlxUIState;
+import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
 import flixel.util.FlxTimer;
+import flixel.addons.transition.Transition;
 import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.FlxTransitionSprite;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileCircle;
+import flixel.addons.transition.FlxTransitionSprite.TransitionStatus;
+import flixel.addons.transition.TransitionData;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxGradient;
 import flixel.FlxSubState;
 import flixel.FlxSprite;
+import flixel.graphics.FlxGraphic;
 import flixel.FlxCamera;
 
 class CustomFadeTransition extends MusicBeatSubstate {
@@ -31,33 +38,40 @@ class CustomFadeTransition extends MusicBeatSubstate {
 		var height:Int = Std.int(FlxG.height / zoom);
 		transGradient = new FlxSprite().makeGraphic(width, height, FlxColor.BLACK);
 		transGradient.scrollFactor.set();
-		add(transGradient);
+		//add(transGradient);
 
 		transBlack = new FlxSprite().makeGraphic(width, height + 400, FlxColor.BLACK);
 		transBlack.scrollFactor.set();
-		add(transBlack);
+		//add(transBlack);
 
 		transGradient.x -= (width - FlxG.width) / 2;
 		transBlack.x = transGradient.x;
 
-		if(isTransIn) {
-			transGradient.y = transBlack.y - transBlack.height;
-			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.linear});
-		} else {
-			transGradient.y = -transGradient.height;
-			transBlack.y = transGradient.y - transBlack.height + 50;
-			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.linear});
-		}
+		//if(isTransIn) {
+		//	transGradient.y = transBlack.y - transBlack.height;
+		//	FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+		//		onComplete: function(twn:FlxTween) {
+		//			close();
+		//		},
+		//	ease: FlxEase.linear});
+		//} else {
+		//	transGradient.y = -transGradient.height;
+		//	transBlack.y = transGradient.y - transBlack.height + 50;
+		//	leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+		//		onComplete: function(twn:FlxTween) {
+		//			if(finishCallback != null) {
+		//				finishCallback();
+		//			}
+		//		},
+		//	ease: FlxEase.linear});
+		//}
+		var transData = new TransitionData(cast "tiles", 0xFF000000, duration, new FlxPoint(0,1));
+		transData.tileData = {width: 32, height: 32, asset: FlxGraphic.fromBitmapData(new GraphicTransTileCircle(0, 0, true, 0xFF000000))};
+		var transitional = new Transition(transData);
+		add(transitional);
+		new FlxTimer().start(duration, function(tmr:FlxTimer) {if(!isTransIn && finishCallback != null)finishCallback(); else close();});
+		//transitional.setStatus(isTransIn?TransitionStatus.OUT:TransitionStatus.IN);
+		transitional.start(isTransIn?TransitionStatus.OUT:TransitionStatus.IN);
 
 		if(nextCamera != null) {
 			transBlack.cameras = [nextCamera];
