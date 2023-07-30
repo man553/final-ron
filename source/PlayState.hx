da21850e882224a574c73a628161eaca8a380767
karbis-alt <mainsameyes@gmail.com>
2023-07-30 18:35:17 +0300

    move icon a lil bit
    U know sometimes u just gotta move the icon u know

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1750,13 +1750,13 @@ class PlayState extends MusicBeatState
 		}
 
 		iconP1 = new HealthIcon(boyfriend.healthIcon, true);
-		iconP1.y = healthBar.y - 75;
+		iconP1.y = healthBar.y - 60;
 		iconP1.visible = !ClientPrefs.hideHud;
 		iconP1.alpha = ClientPrefs.healthBarAlpha;
 		add(iconP1);
 
 		iconP2 = new HealthIcon(dad.healthIcon, false);
-		iconP2.y = healthBar.y - 75;
+		iconP2.y = healthBar.y - 60;
 		iconP2.visible = !ClientPrefs.hideHud;
 		iconP2.alpha = ClientPrefs.healthBarAlpha;
 		add(iconP2);
